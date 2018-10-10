be82d605c0506fbe699f1665e5311531f67faf54
philipwisner <philipwisner@gmail.com>
2018-10-09 20:50:42 -0400

    updated packages and mongoose

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ const flash = require('connect-flash');
 require("dotenv").config();
 
 
-
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true
+});
 // mongoose.connect('mongodb://localhost:27017/farm-voyage-development');
 
 //DEFINE EXPRESS APP
